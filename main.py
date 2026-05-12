f243b434222cced0be6bd3d36ed4ba7ed3f7e0f1
joao-s-rocha <joao.roha25@gmail.com>
2026-05-12 17:15:34 -0400

    Rotation added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
 
     while True:
         log_state()
+        dt = (clock.tick(60) / 1000)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,8 +23,9 @@ def main():
                 
         screen.fill("black")        
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()        
-        dt = (clock.tick(60) / 1000)
+        
 
 
 if __name__ == "__main__":
