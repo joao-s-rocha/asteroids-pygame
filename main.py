6da13ab53704b6019b05d452466a08ee9c6cac51
joao-s-rocha <joao.roha25@gmail.com>
2026-05-12 15:44:03 -0400

    Adding the player class and in the window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -17,11 +19,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        pygame.display.flip()
+                
+        screen.fill("black")        
+        player.draw(screen)
+        pygame.display.flip()        
         dt = (clock.tick(60) / 1000)
-        print(f"value of dt {dt}")
 
 
 if __name__ == "__main__":
