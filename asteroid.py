f4893477221261f6673c317c2f5ed28fca83ee27
joao-s-rocha <joao.roha25@gmail.com>
2026-05-14 17:25:07 -0400

    Shooting and destroying asteroids

--- asteroid.py
+++ asteroid.py
@@ -4,15 +4,8 @@ from circleshape import CircleShape
 from constants import *
 
 class Asteroid(CircleShape):
-    DIRECTIONS = ["up", "stoped", "down"]
-
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-
-        self.random_direction_to_move = {
-            "x_change": random.choice(self.DIRECTIONS), 
-            "y_change": random.choice(self.DIRECTIONS)
-        }
 
     def draw(self, screen):
         pygame.draw.circle(
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -32,9 +34,25 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        
+        self.shot_cooldown -= dt
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+    
+    def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+
+        vector = pygame.Vector2(0, 1)
+        my_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        my_shot.velocity = vector.rotate(self.rotation)
+        my_shot.velocity = my_shot.velocity * PLAYER_SHOOT_SPEED
+        
